37d6c571f75d78a277526d4df1ef20ab6c3680a9
Sadique <sadiquemecheri@gmail.com>
2023-03-21 16:16:35 +0530

    first commit

---
+++ news_and_blogs/lib/constants/sample_data.dart
@@ -0,0 +1,6 @@
+const List carosi = [
+      "https://media.istockphoto.com/id/1412282189/photo/lock-network-technology-concept.jpg?b=1&s=170667a&w=0&k=20&c=He6JoCWuNNp7I9vjOUzELTax-hDay-1BnO6VyFxu8Po=",
+      // "https://static.businessworld.in/article/article_extra_large_image/1464784400_qDXtLH_businessdeals.jpg"
+      "https://t4.ftcdn.net/jpg/01/98/83/09/360_F_198830916_gFI9RYVWj7iHiJgmb83j7xVtbNWLpcsL.jpg"
+       ""
+    ];
